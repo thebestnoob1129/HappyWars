2b6b690fee74e096b758a5809de4bd6a5ebe234c
TheBestNoob <73605149+thebestnoob1129@users.noreply.github.com>
2025-01-28 17:25:43 -0500

    Implementation

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,30 +4,47 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    float speed = 1;
+    [SerializeField] public float speed = 1;
 
-    CharacterController charcont;
-    
+    InputAction moveAction;
+    PlayerInput playerInput;
+    Rigidbody rb;
+    Camera cam;
     
     void Start()
     {
-        charcont = GetComponent<CharacterController>();
+        // Rigidbody
+        rb = GetComponent<Rigidbody>();
+        if (rb is null) { Debug.LogError(transform.name + ": Rigidbody is null"); }
+        
+        // Player Camera
+        cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        if (cam is null) { Debug.LogError(transform.name + ": Camera is null ( How did we get here)"); }
+        
+        //Player Actions
+        playerInput = GetComponent<PlayerInput>();
+        if (playerInput is null) { Debug.LogError(transform.name + ": Actions is null"); }
+        
+        moveAction = playerInput.actions.FindAction("Move");
     }
-
-    
+    /*
+    private void OnEnable()
+    {
+        playerInput.actions.Enable();
+    }
+    private void OnDisable()
+    {
+        playerInput.actions.Disable();
+    }
+    */
     void Update()
     {
-        
-    }
-
-    
-    public void Move(InputAction.CallbackContext context)
-    {
-        var dir = context.ReadValue<Vector2>();
-
-        charcont.Move(new Vector3(dir.x * speed, 0, dir.y * speed));
+        //Player movement to Variable
+        Vector2 dir = moveAction.ReadValue<Vector2>();
+        if (dir.y > 0) { rb.linearVelocity = new Vector3(cam.transform.forward.x, 0, cam.transform.forward.z) * speed; }
+        if (dir.y < 0) { rb.linearVelocity = new Vector3(-cam.transform.forward.x, 0, -cam.transform.forward.z) * speed; }
+        if (dir.x > 0) { rb.linearVelocity = new Vector3(cam.transform.right.x, 0, cam.transform.right.z) * speed; }
+        if (dir.x < 0) { rb.linearVelocity = new Vector3(-cam.transform.right.x, 0, -cam.transform.right.z) * speed; }
 
     }
-    
-
 }
